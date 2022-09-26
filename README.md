bf8b1be32b51222584f00b193aa91a0cb2dd392f
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-09-26 23:52:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Esto es una especie de base de datos que he hecho para simular la de una tienda, con diferentes productos de diferentes categorías. No encontraba ninguna en castellano así que me hice una yo para probar en mis proyectos de frontend
+
+Está basado en https://fakestoreapi.com
+
+La imágenes las he sacado de Google, no sé si tienen copyright o no (probablemente si)
---
+++ store.js
@@ -0,0 +1,470 @@
+export const products = [
+  {
+    "id": 1,
+    "title": "Crema Nivea",
+    "price": 3.50,
+    "description": "Una buena crema.",
+    "category": "Otros",
+    "image": "https://m.media-amazon.com/images/I/61t5b29Y4rL._AC_SL1500_.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 9.8,
+      "count": 1536
+    }
+  },
+  {
+    "id": 2,
+    "title": "Acer Nitro 5",
+    "price": 1499,
+    "description": "Intel Core i7-11800H, 16GB, 512GB SSD, 3050Ti, 15.6 pulgadas.",
+    "category": "Electrónica",
+    "image": "https://img.pccomponentes.com/articles/1037/10370488/1688-acer-nitro-5-an515-57-75m9-intel-core-i7-11800h-16gb-512gb-ssd-rtx-3050ti-156.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 9.5,
+      "count": 25
+    }
+  },
+  {
+    "id": 3,
+    "title": "Camiseta antisocial",
+    "price": 16.99,
+    "description": "No soy antisocial, soy antiestúpidos",
+    "category": "Ropa",
+    "image": "https://statics.pampling.com/imagenes/galerias/imagen_36135.jpg?1664032400",
+    "stock": true,
+    "rating": {
+      "rate": 8.7,
+      "count": 215
+    }
+  },
+  {
+    "id": 4,
+    "title": "El señor de las moscas",
+    "price": 8.75,
+    "description": "Un buen libro",
+    "category": "Libros",
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTyV8Rmq3yZULQJ1mIRYWiBJC6jqPKAjvm7kngway4w0ykfxLl8RCmh66wJzsfQmncqt-zbhCYJsDAAic4Wp2GXA663u1CeezT_mvnikpZNB7mIYaWw7QGUtQ&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.8,
+      "count": 32
+    }
+  },
+  {
+    "id": 5,
+    "title": "Zapatillas",
+    "price": 19.99,
+    "description": "Marca Lidl",
+    "category": "Ropa",
+    "image": "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2020/10/14/16026692592374.jpg",
+    "stock": false,
+    "rating": {
+      "rate": 12,
+      "count": 666
+    }
+  },
+  {
+    "id": 6,
+    "title": "Tableta gráfica",
+    "price": 59.99,
+    "description": "Para dibujar",
+    "category": "Electrónica",
+    "image": "https://m.media-amazon.com/images/I/610tdKooO-L._AC_SS450_.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 7.8,
+      "count": 213
+    }
+  },
+  {
+    "id": 7,
+    "title": "Harry Potter",
+    "price": 159.99,
+    "description": "Saga completa",
+    "category": "Libros",
+    "image": "https://images-na.ssl-images-amazon.com/images/I/71EiOIwH7cL.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 9.6,
+      "count": 1532
+    }
+  },
+  {
+    "id": 8,
+    "title": "Vestido",
+    "price": 12,
+    "description": "Vestido magenta",
+    "category": "Ropa",
+    "image": "https://m.media-amazon.com/images/I/61MWCK7c7kL._AC_UX466_.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 8.4,
+      "count": 63
+    }
+  },
+  {
+    "id": 9,
+    "title": "Semillas de tomate",
+    "price": 1.25,
+    "description": "Semillas de tomate",
+    "category": "Jardinería",
+    "image": "https://theoriginalgarden.com/Argazkiak/Fotos/20180326171124.jpg",
+    "stock": false,
+    "rating": {
+      "rate": 8.3,
+      "count": 200
+    }
+  },
+  {
+    "id": 10,
+    "title": "Collar",
+    "price": 569,
+    "description": "Collar",
+    "category": "Otros",
+    "image": "https://cdn-eu-ec.yottaa.net/609426b74f1bbfff95ac5a83/es.pandora.net/v~4b.16/dw/image/v2/BFCR_PRD/on/demandware.static/-/Sites-pandora-master-catalog/default/dw69e9ff1c/images/productimages/main/390055C01_RGB.jpg?sw=900&sh=900&sm=fit&sfrm=png&bgcolor=F5F5F5&yocs=9_e_",
+    "stock": true,
+    "rating": {
+      "rate": 7.6,
+      "count": 63
+    }
+  },
+  {
+    "id": 11,
+    "title": "Por si las voces vuelven",
+    "price": 12.50,
+    "description": "Por si las voces vuelven",
+    "category": "Libros",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQ-cUqpipzxIe-0ZZMZTw3uMG6cBFANV3pHt2_P2zHlEF47RXMn1vtSkZKpzwBba9HO2tFYYsMgHKAyoUnaqeDKZL8LKQLyDCxAs8Y5Tuw&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.9,
+      "count": 1503
+    }
+  },
+  {
+    "id": 12,
+    "title": "2 x RAM",
+    "price": 59.99,
+    "description": "RAM",
+    "category": "Electrónica",
+    "image": "https://thumb.pccomponentes.com/w-530-530/articles/16/164469/1.jpg",
+    "stock": false,
+    "rating": {
+      "rate": 8.8,
+      "count": 6
+    }
+  },
+  {
+    "id": 13,
+    "title": "Tacones",
+    "price": 699,
+    "description": "Tacones",
+    "category": "Ropa",
+    "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSLC_QmQbqpBy8DXqTjUKH9DU1KhDIy0HDNW95HlztokjUyjhTzL1E1DL6bt8Zs92RvaX75qBBsLaRxixCHdWRihJGbFP6sSi7OU07cJziKN6xtLbjHEHjRl-c&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.4,
+      "count": 63
+    }
+  },
+  {
+    "id": 14,
+    "title": "Herramientas de jardinería",
+    "price": 29.75,
+    "description": "Herramientas de jardinería",
+    "category": "Jardinería",
+    "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTfugYPrtEe64sfOxOnYTzuW-dOW6_5Pu280FTrX2OnZBhqhcDbNht2BGR2tMJS-c9irbbKIkp-CWz60bSzf_VzHOYzlBgBV1oUAr6ZX4ykj0mBUHCUxrcQ&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 6.7,
+      "count": 23
+    }
+  },
+  {
+    "id": 15,
+    "title": "¡Guardas! ¡Guardas!",
+    "price": 11.99,
+    "description": "¡Guardas! ¡Guardas!",
+    "category": "Libros",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRS2bg8CwaPBhTQWrIC1Ji9q3M1a8KlsaxKUzQnj5DK1g4BcZYGJSSqXxb8q-ngXnyHfD9NV8CyFLkLPXU8ZRdrxZ5NUA0JSi_bD4o0N8kL&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.8,
+      "count": 6334
+    }
+  },
+  {
+    "id": 16,
+    "title": "Cuaderno",
+    "price": 3.69,
+    "description": "Cuaderno",
+    "category": "Otros",
+    "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTiZKkVgYIb9obxxqKMceLsRS8X0958Biv9qKfRlMmunRMhkeYKbI3NiGoRqoSI6uACilvrotNBubVjFntF66vrGlEL4FFBSgj7Z6GeDRfBblxrl2NeCrCn&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 5.9,
+      "count": 651
+    }
+  },
+  {
+    "id": 17,
+    "title": "Vapeador",
+    "price": 45.75,
+    "description": "Vapeador",
+    "category": "Electrónica",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQNl7RmFlWWIVaTZmd1PBCoDANQ8KLaNroK5cnBrw3oRWNOG26BPqyz2eje4P1S7zZ3qAvJN0NIzdoAYbI-QMbp0w6w9jWrMqVMD8g8WmJWtsa4bUZJ3GrI87uL&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 4.7,
+      "count": 126
+    }
+  },
+  {
+    "id": 18,
+    "title": "Maceta",
+    "price": 2.99,
+    "description": "Maceta",
+    "category": "Jardinería",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSQraVmRtJa4eMwvCudgzZesVgol2s9v0o2DHWNT8eC-a5O9kC58kOt3f5ZCnVAXimifhgVgUnTgbQqMiS8uO5imhMHZgWEjbqLv7SACHnPtj6KYME_mG45&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.0,
+      "count": 1547
+    }
+  },
+  {
+    "id": 19,
+    "title": "Camiseta Linux",
+    "price": 15,
+    "description": "Camiseta Linux",
+    "category": "Ropa",
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRF8IR-iJnofrygZVYmIR4lY8zFrkS5KYYqNs88bScritKuDBhoNE4OH9EUPTVHRGFyqjc0HZEUEFH0LfyGHqWJB3aJ5VGu9pMGRgqyJkca1j3QnP63oEbW&usqp=CAE",
+    "stock": false,
+    "rating": {
+      "rate": 9.8,
+      "count": 6514
+    }
+  },
+  {
+    "id": 20,
+    "title": "Anillo",
+    "price": 79.79,
+    "description": "Anillo",
+    "category": "Otros",
+    "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTNsT1t4z_UkBYJcl7mMZETjCnk7kWm5vN1jwRkc63YnAPNnmUdBRarFwaD4Ni6pb9zhdvQ0N8S2w7S_Got-vBJUfwfYI5cGgV5oGShjMZe9Gj04TZ-zp1YlEeP&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.9,
+      "count": 5
+    }
+  },
+  {
+    "id": 21,
+    "title": "Mort",
+    "price": 13.69,
+    "description": "Mort",
+    "category": "Libros",
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ3hWCsal6zwy4mftPp60gAKwCCIQVTXI0g0PPES0KduUYBfZaDLoxJby9ApuqXTJ6aitZOE6YwHgzlGLNVesRyBXORSTJwBb13D9p-um8&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.9,
+      "count": 45
+    }
+  },
+  {
+    "id": 22,
+    "title": "Rosal mini",
+    "price": 5.99,
+    "description": "Rosal mini",
+    "category": "Jardinería",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSmaovHYuDxRwovP7ZQZz9-IGq3kmzcjBaBtCgNrYY0kL5aOKZB4ACYponLvQgG4qjQIihQ6MN9gW9Cz9KNPmlBGcTpkneWf7DkZi3fAX7HEPMajNQ_USr_Lg&usqp=CAE",
+    "stock": false,
+    "rating": {
+      "rate": 8.7,
+      "count": 55
+    }
+  },
+  {
+    "id": 23,
+    "title": "Semillas de césped",
+    "price": 5.65,
+    "description": "Semillas de césped",
+    "category": "Jardinería",
+    "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ_rsVXuxNooHshj9-DiPAyJSdfxsA78BuWWT33UXii1f20sx0-eqH6MUCflOQsV6fdonMyc-cB6YDO3PeFTSFGmWeV-N0LN3n_FLjUodDiPyOQfaIFsPiW&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.9,
+      "count": 321
+    }
+  },
+  {
+    "id": 24,
+    "title": "Tarjeta gráfica",
+    "price": 599.99,
+    "description": "Tarjeta gráfica",
+    "category": "Electrónica",
+    "image": "https://img.pccomponentes.com/articles/43/432064/1343-gigabyte-geforce-rtx-3060-gaming-oc-12gb-gddr6-rev-20.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 4.6,
+      "count": 5
+    }
+  },
+  {
+    "id": 25,
+    "title": "Camiseta de hora de aventuras",
+    "price": 45.99,
+    "description": "Camiseta de hora de aventuras",
+    "category": "Ropa",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRqbZbu0BwKvoWQGWmSJNwrML17aKZfUllctyNTxrAUTQKXJq_zJnsjMsd5qhKJpKy2DRYYiYKbjA4WlK4V1T4U-SMDTikm3rwb7jMMSKufDUtqjRWrB_UThw&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.1,
+      "count": 44
+    }
+  },
+  {
+    "id": 26,
+    "title": "Bolso",
+    "price": 5.99,
+    "description": "Bolso",
+    "category": "Otros",
+    "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTbkibPZUGpxoscaXrVh2r0VIMG3UTQzMk7nxLxorbAN-xmJHXZQorgz3lKd9jxI6yC8YVOuat4P2sw1_chn2XaKYz8VZ0qChfUmjBPgQ0FV1MoZDoO88njUuQ&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 5.7,
+      "count": 645
+    }
+  },
+  {
+    "id": 27,
+    "title": "Aguacate",
+    "price": 32.77,
+    "description": "Aguacate",
+    "category": "Jardinería",
+    "image": "https://cdn2.planetahuerto.es/estaticos/imagenes/ficha/121822/121822_1.jpg?fit=crop&w=256&h=256&auto=format&q=100",
+    "stock": true,
+    "rating": {
+      "rate": 9,
+      "count": 45
+    }
+  },
+  {
+    "id": 28,
+    "title": "Portátil",
+    "price": 1599,
+    "description": "Portátil",
+    "category": "Electrónica",
+    "image": "https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/inspiron-notebooks/15-3520/media-gallery/in3520-xnb-01-sl.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=402&qlt=100,1&resMode=sharp2&size=402,402&chrss=full",
+    "stock": false,
+    "rating": {
+      "rate": 7.7,
+      "count": 6
+    }
+  },
+  {
+    "id": 29,
+    "title": "Pulsera",
+    "price": 455.99,
+    "description": "Pulsera",
+    "category": "Otros",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTVOeNkoJ8VitwNrlFwkweTnmANVjVajVmVmtymmZCaEsID_Knr1sSbaD1fakibdcHFLndi1f2-ZFcCEFQSLmXtLbOCWa8ibg&usqp=CAY",
+    "stock": true,
+    "rating": {
+      "rate": 8.7,
+      "count": 2
+    }
+  },
+  {
+    "id": 30,
+    "title": "Naranjo",
+    "price": 15.45,
+    "description": "Naranjo",
+    "category": "Jardinería",
+    "image": "https://decoandlemon.com/media/catalog/product/cache/3ad20444762bbdde6309c9935a0d8d33/7/4/74020033_1.jpg",
+    "stock": true,
+    "rating": {
+      "rate": 8,
+      "count": 44
+    }
+  },
+  {
+    "id": 31,
+    "title": "El color de la magia",
+    "price": 14.55,
+    "description": "El color de la magia",
+    "category": "Libros",
+    "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRAovZponiUVlo7iDcgOTS5BzYGtXIi1xYEM0TgjqeW5wVN4KwtjjI44aZ_srznwC4TcW4c0Uee2O_ZwVtcJmVO8ni4rLd-deuzyipyd0wIxO-EIzCgGYd7Qw&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 9.7,
+      "count": 5345
+    }
+  },
+  {
+    "id": 32,
+    "title": "Ebook",
+    "price": 23.99,
+    "description": "Ebook",
+    "category": "Electrónica",
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT4Zjn6A957H_o9yWKj14dVnudevuzclBWDtqUovF8zE62f1oTmW0nuTM8LxnW0j37upUXpalvUM4fYKKUgDmXEr072L4jsK01gYt_jRffApkafrOyPKaCuTA&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 7.0,
+      "count": 44
+    }
+  },
+  {
+    "id": 33,
+    "title": "Agenda",
+    "price": 5.60,
+    "description": "Agenda",
+    "category": "Otros",
+    "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTzgANDfQhq9YTZACQXTtwQn62l16NHCCApszF7SUh9XmEsZeVikuERkNnHWYneJ8jtCrqKOJ2UpQi6vM4274UKdsM_EGzishuAbDN-UmBg1N1XzjRAnRVQ&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.0,
+      "count": 4377
+    }
+  },
+  {
+    "id": 34,
+    "title": "Smartwatch",
+    "price": 45.99,
+    "description": "Smartwatch",
+    "category": "Electrónica",
+    "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSXw7_EkpwH2VyLTNNJ5WC0bZ6b6mI9xu7vb7z6IetVWIFJS1J7nZMWsGxSH2-csv0EOZn520eFzCeRjJaCV13D6CD6K1Tbo4ySzXfs-n46sf-HUMFTljQZ",
+    "stock": true,
+    "rating": {
+      "rate": 6.7,
+      "count": 47
+    }
+  },
+  {
+    "id": 35,
+    "title": "Azucenas",
+    "price": 7.99,
+    "description": "Azucenas",
+    "category": "Jardinería",
+    "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRVLFW4XzOP-1d8gvhnI9RiYXxlAXjFGsuiUTdnoeSmtjgz7iZptUkAxZRLo--nhecn4fOaIZ8FoeAIe8d0i30yeaZrGy0CHiu1R1ymvSQUkbulmFIg8IdP&usqp=CAE",
+    "stock": true,
+    "rating": {
+      "rate": 8.8,
+      "count": 456
+    }
+  },
+  {
+    "id": 36,
+    "title": "Manguera",
+    "price": 17.99,
+    "description": "Manguera",
+    "category": "Jardinería",
+    "image": "https://cdn.shopify.com/s/files/1/0249/3674/2966/products/7_5mexpandablegardenhosewithspraygun_1_2000x.jpg?v=1650146713",
+    "stock": true,
+    "rating": {
+      "rate": 9.6,
+      "count": 4516
+    }
+  }
+]
